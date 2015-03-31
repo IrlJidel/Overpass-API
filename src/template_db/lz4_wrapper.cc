2e87ecb09aa542bb9ffc46436567aaa0d6f37ca4
mmd-osm <mmd.osm@gmail.com>
2015-03-31 18:54:56 +0200

    lz4: prototype contd.

--- src/template_db/lz4_wrapper.cc
+++ src/template_db/lz4_wrapper.cc
@@ -1,6 +1,7 @@
 #include "lz4_wrapper.h"
 
 #include <cstdlib>
+#include <cstring>
 #include <exception>
 #include <iostream>
 #include <sstream>
@@ -13,12 +14,18 @@ LZ4_Deflate::~LZ4_Deflate() { }
 int LZ4_Deflate::compress(const void* in, int in_size, void* out, int out_buffer_size)
 {
    int ret = LZ4_compress_limitedOutput((const char*) in, (char *) out + 4, in_size, out_buffer_size - 4);
-   if (ret == 0)
-     throw Error(-5); // Too few output space
+   if (ret == 0)    // buffer is too small, skip compression
+   {
+     if (in_size > out_buffer_size - 4)
+       throw Error (-5);     // buffer too small, abort processing
 
-   *(unsigned int*)out = ret;
+     *(int*)out = in_size * -1;
+     std::memcpy ((char *) out + 4, (const char*)in, in_size);
+     ret = in_size;
+   }
+   else
+     *(int*)out = ret;
 
-   //std::cout << "compress: " << in_size << " --> " << ret << "\n";
    return ret;
 }
 
@@ -29,12 +36,23 @@ LZ4_Inflate::~LZ4_Inflate() { }
 
 int LZ4_Inflate::decompress(const void* in, int in_size, void* out, int out_buffer_size)
 {
-  int in_size_compressed = *(unsigned int*)in;
-  int ret = LZ4_decompress_safe((const char*) in + 4, (char*) out, in_size_compressed, out_buffer_size);
-//  std::cout << "decompress: " << in_size << " ~ " << in_size_compressed << " --> " << ret << "\n";
-  if (ret < 0)
-    throw Error (ret);
+  int ret;
+  int in_buffer_size = *(int*)in;
 
+  if (in_buffer_size > 0)
+  {
+    ret = LZ4_decompress_safe((const char*) in + 4, (char*) out, in_buffer_size, out_buffer_size);
+    if (ret < 0)
+      throw Error (ret);
+  }
+  else
+  {
+    if ((in_buffer_size * -1) > out_buffer_size)
+      throw Error (1);
+
+    std::memcpy ((char*) out, (const char*) in + 4, in_buffer_size * -1);
+    ret = in_buffer_size * -1;
+  }
   return ret;
 }
 
